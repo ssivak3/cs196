81ed15d3c9e01e2982b0b07bc1f78eec9ae795ae
ssivak3 <35472899+ssivak3@users.noreply.github.com>
2018-04-01 16:48:51 -0500

    Add files via upload

--- forms.py
+++ forms.py
@@ -7,4 +7,3 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-    
