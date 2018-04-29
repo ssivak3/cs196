bf860e9ee4a32e67b29a9abbeb149396d0ae901e
OmarK807 <omark807@gmail.com>
2018-04-29 18:18:43 -0500

    updates

--- app/routes.py
+++ app/routes.py
@@ -18,10 +18,13 @@ from flask import Flask, render_template, request
 # def render(cadenza, template_folder = 'musicpage.html'):
 #     # insert here
 
+
+# Reads the HTML from "musicpage.html" and processes it to generate notes.
 @app.route('/')
 def music_page():
     return render_template('musicpage.html', notes="")
 
+# Renders template for the music score to be displayed.
 @app.route('/', methods=['POST'])
 def music_page_post():
     text = request.form['text']
